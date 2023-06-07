fb7db8cecf381e1b82d28e58c007798d88a06435
Ben Corner <ultimatekiwi+git@gmail.com>
2023-06-07 05:11:42 -0500

    oops I need to be careful with backticks? and learn about commit messages

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `bcorner github website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
