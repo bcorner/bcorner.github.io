d0d184880485ed266ce5f96417c64686cdc73e96
Ben Corner <ultimatekiwi+git@gmail.com>
2023-07-26 14:07:40 -0500

    basic text-only index via gatsby tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+// step uno: Import React
+import * as React from 'react'
+
+// step numero dos: define my component
+const AboutPage = () => {
+    return (
+	<main>
+	<h1>About Me</h1>
+	<p> I am a stranger loop? </p>
+	</main>
+    )
+}
+
+export const Head = () => <title>About me</title>
+
+// step numero tres: export my component
+export default AboutPage
+	
